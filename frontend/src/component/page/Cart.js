e67eb8633fa540cd79812bba6a8f73f6dbcd9d60
Mahrukh2001 <mahrukhtariq2001@gmail.com>
2023-10-30 19:39:51 +0500

    Update files of frontend

--- frontend/src/component/page/Cart.js
+++ frontend/src/component/page/Cart.js
@@ -48,76 +48,72 @@ const Cart = () => {
         },1000)
     }
         
-    }
+  }
   return (
-  
-  
-  <div className='p-4 md:p-8'>
+    <>
+    
+    <div className='p-4 md:p-8'>
       <h2 className='text-xl md:text-2xl font-bold text-slate-600 mb-4'>Your Cart Items</h2>
-
-        {
-            productCartItem[0] ?
-
-         <div className='flex flex-wrap gap-4'>
-            {/* display cart items */}
-            <div className='w-full md:w-2/3'>
-                {
-                    productCartItem.map(el=>{
-                        return(
-                            <CartProduct
-                             key={el._id}
-                             id={el._id}
-                             name={el.name}
-                             image={el.image}
-                             category={el.category}
-                             qty={el.qty}
-                             total={el.total}
-                             price={el.price}
-                             
-                             />
-                        )
-                    })
-                }
-                
-                
-            </div>
-
-            {/* total cart items */}
-            <div className='w-full md:w-1/3'>
-                <h2 className='bg-blue-500 text-white p-2 text-lg mb-4'>Order Summary</h2>
-                <div className='flex w-full py-2 border-b'>
-                  <p>Total Qty:</p>
-                  <p className='ml-auto font-bold'>{totalQty}</p>
-                </div>
-                <div className='flex w-full py-2 border-b'>
-                  <p>Total Price:</p>
-                  <p className='ml-auto font-bold'>
-                     <span className='text-red-500 px-1'>₨</span> {totalPrice}
-                  </p>
-            </div>
-            <button
-              className='bg-red-500 w-full text-lg font-bold py-2 text-white cursor-pointer'
-              onClick={handlePayment}
-            >
-              Payment
-            </button>
-            </div>
-
-        </div>
-        :<>
-
-        <div className='flex justify-center items-center flex-col'>
-          <img src={emptyCartImage} className='w-full max-w-sm' alt='Empty Cart' />
-          <p className='text-slate-500 text-xl md:text-2xl font-bold mt-4'>Empty Cart</p>
-        </div>
-
-          
-        </>
-}
-    </div>
-
-  </>
-  )
-}
-
-export default Cart
+          {
+              productCartItem[0] ?
+  
+              <div className='flex flex-wrap gap-4'>
+                 {/* display cart items */}
+                <div className='w-full md:w-2/3'>
+             
+             
+                  {
+                      productCartItem.map(el=>{
+                          return(
+                              <CartProduct
+                               key={el._id}
+                               id={el._id}
+                               name={el.name}
+                               image={el.image}
+                               category={el.category}
+                               qty={el.qty}
+                               total={el.total}
+                               price={el.price}
+                               
+                               />
+                          )
+                      })
+                  }
+                  
+                  
+              </div>
+  
+              {/* total cart items */}
+              <div className='w-full md:w-1/3'>
+                  <h2 className='bg-blue-500 text-white p-2 text-lg mb-4'>Order Summary</h2>
+                  <div className='flex w-full py-2 border-b'>
+                     <p>Total Qty:</p>
+                     <p className='ml-auto font-bold'>{totalQty}</p>
+                  </div>
+                  <div className='flex w-full py-2 border-b'>
+                    <p>Total Price:</p>
+                    <p className='ml-auto font-bold'>
+                      <span className='text-red-500 px-1'>₨</span> {totalPrice}
+                    </p>
+                  </div>
+                  <button className='bg-red-500 w-full text-lg font-bold py-2 text-white cursor-pointer' onClick={handlePayment}>Payment</button>
+              </div>
+  
+          </div>
+          :<>
+  
+          <div className='flex justify-center items-center flex-col'>
+              <img src={emptyCartImage} className='w-full max-w-sm'/>
+              <p className='text-slate-500 text-xl md:text-2xl font-bold mt-4'>Empty Cart</p>
+          </div>
+  
+            
+          </>
+  }
+      </div>
+  
+    </>
+    )
+  }
+  
+  export default Cart
